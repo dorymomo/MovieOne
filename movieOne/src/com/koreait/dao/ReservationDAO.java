2cbbc95f23ff31ffc8dae28af14c7c8424801463
haejun47 <lhjun4780@gmail.com>
2026-02-04 08:48:24 +0900

    2026-02-04 08:48 lhj

--- movieOne/src/com/koreait/dao/ReservationDAO.java
+++ movieOne/src/com/koreait/dao/ReservationDAO.java
@@ -134,10 +134,10 @@ public class Reservation {
 	//예매 하는 메소드 
 	//매개변수는 영화 번호, 회원 번호, 상영날짜 
 	//반환 타입은 boolean 영화 예매 성공 여부 
-	public boolean reservation(int mvNum, int memNum, String showDate) {
-		
-	
-	}
+//	public boolean reservation(int mvNum, int memNum, String showDate) {
+//		+
+//	
+//	}
 	
 	
 	
