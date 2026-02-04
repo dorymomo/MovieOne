df39db95c1f09dfbac6aa82ff60f12eedd947ded
xxxnoname001 <xxktwxx0228@gmail.com>
2026-02-04 17:29:26 +0900

    20260204 김태우 영화DAO(장르조회, 영화추가)작성 완료

--- movieOne/src/com/koreait/dao/MovioDAO.java
+++ movieOne/src/com/koreait/dao/MovioDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.koreait.dto.MovieDTO;
 
@@ -105,4 +106,111 @@ public class MovioDAO {
 		}
 		return false;
 	}
+	
+	
+
+	/** 
+	 * @param movieDTO
+	 * @author ktw
+	 * @since jdk-17
+	 * @return boolean
+	 * 
+	 * <p>영화 장르 조회(장르를 검색시에 해당 카테고리에 있는 영화 리스트 반환)
+	 *  입력을 받는걸 만들고, 장르가 반환되게 해야함</p>
+	 */
+
+		
+		public List<MovieDTO> mvGenreList(String genre) {
+
+		    List<MovieDTO> list = new ArrayList<>();
+
+		    String qeury =
+		        "SELECT mv_title, mv_genre, mv_price, mv_location " +
+		        "FROM tbl_movie " +
+		        "WHERE mv_genre = ?";
+
+		    try {
+		        connection = DBConnector.getConnection();
+		        preparedStatement = connection.prepareStatement(qeury);
+
+		        // 장르 바인딩 (예: "애니메이션", "sf", "드라마")
+		        preparedStatement.setString(1, genre);
+
+		        resultSet = preparedStatement.executeQuery();
+
+		        while (resultSet.next()) {
+		            MovieDTO movie = new MovieDTO();
+		            movie.setMvTitle(resultSet.getString("mv_title"));
+		            movie.setMvGenre(resultSet.getString("mv_genre"));
+		            movie.setMvPrice(resultSet.getInt("mv_price"));
+		            movie.setMvLocation(resultSet.getString("mv_location"));
+		            list.add(movie);
+		        }
+
+		    } catch (SQLException e) {
+		        System.out.println("mvGenreList() SQL 오류");
+		        e.printStackTrace();
+		    } finally {
+		        try {
+		            if (resultSet != null) resultSet.close();
+		            if (preparedStatement != null) preparedStatement.close();
+		            if (connection != null) connection.close();
+		        } catch (SQLException e) {
+		            System.out.println("mvGenreList() 자원 해제 오류");
+		            e.printStackTrace();
+		        }
+		    }
+
+		    return list;
+		}
+	
+		
+		/** 
+		 * @param movieDTO
+		 * @author ktw
+		 * @since jdk-17
+		 * @return boolean
+		 * 
+		 * <p>영화 추가(사용자 입장에서 볼 영화 추가하는 식의 메소드)
+		 *  그냥 사용자가 전부 입력해서 추가하는 느낌 영화테이블에 추가되는거임 그렇게 진행하도록 함</p>
+		 */
+
+		   public boolean mvInsert(MovieDTO movieDTO) {
+			      String query = "INSERT INTO tbl_movie" + "VALUES (SEQ_STUDENT.NEXTVAL, ?, ?, ?, ?)";
+			      int result = 0;
+
+			      try {
+			         connection = DBConnector.getConnection();
+			         preparedStatement = connection.prepareStatement(query);
+			         // ?바인딩
+			         preparedStatement.setString(1, movieDTO.getMvTitle());
+			         preparedStatement.setString(2, movieDTO.getMvGenre());
+			         preparedStatement.setInt(3, movieDTO.getMvPrice());
+			         preparedStatement.setString(4, movieDTO.getMvLocation());
+			         // 쿼리 실행
+			         result = preparedStatement.executeUpdate();
+			      } catch (SQLException e) {
+			         // TODO Auto-generated catch block
+			         System.out.println("mvInsert() SQL 오류!!");
+			         e.printStackTrace();
+			      } finally {
+			         try {
+			            if (preparedStatement != null) {
+			               preparedStatement.close();
+			            }
+			            if (connection != null) {
+			               connection.close();
+			            }
+			         } catch (SQLException e) {
+			            // TODO Auto-generated catch block
+			            System.out.println("mvInsert() 연결 종료 오류!");
+			            e.printStackTrace();
+			         }
+
+			      }
+			      return result > 0;
+			   }
+	
+	
+
 }
