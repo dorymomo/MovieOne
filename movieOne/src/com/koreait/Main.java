e5e3d989f7cd9cd2cadef82aa9facd67cc419e95
ChangHoon0524 <hunybear05@gmail.com>
2026-02-04 17:54:20 +0900

    20260204 송창훈 메인 메서드에 추가

--- movieOne/src/com/koreait/Main.java
+++ movieOne/src/com/koreait/Main.java
@@ -1,9 +1,11 @@
 package com.koreait;
 
+import com.koreait.controller.MovieController;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		
+		new MovieController().run();
 	}
 }
